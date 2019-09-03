4e464917ce5fbbaa0cd3a88463f13d5c77d0a1d3
Zachary Seebeck <seebeckzachary@gmail.com>
2019-09-02 21:43:21 -0400

    Minor Changes.

---
+++ app/Http/Controllers/SellableController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sellable;
+use Illuminate\Http\Request;
+
+class SellableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sellable  $sellable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sellable $sellable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sellable  $sellable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sellable $sellable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sellable  $sellable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sellable $sellable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sellable  $sellable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sellable $sellable)
+    {
+        //
+    }
+}
---
+++ database/factories/SellableFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Sellable;
+use Faker\Generator as Faker;
+
+$factory->define(Sellable::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
