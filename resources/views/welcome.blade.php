c06237d7a67a82256244733af5d102fe103c8f7c
Zachary Seebeck <seebeckzachary@gmail.com>
2019-09-04 21:17:45 -0400

    Deleted, reset, and cleaned the project. Also brought it up to Laravel 6 while it was still early enough.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,1 @@
 @extends('layouts.app')
-
-@section('content')
-
-    <div class="container">
-
-        <div class="row">
-            <sellable :img_url="'https://www.hammerheadshop.com/wp-content/uploads/2017/08/Product-ImageCOMING-SOON-2.png'" :title="'sdf'" :price="'sdf'"></sellable>
-        </div>
-
-    </div>
-@endsection
