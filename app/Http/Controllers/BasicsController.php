99195e5863c4c8b4c701f52215b97f9b3c60ce78
Zachary Seebeck <SeebeckZachary@Zacharys-MacBook-Pro.local>
2019-12-30 16:25:25 -0500

    Kinda starting something basic

---
+++ app/Http/Controllers/BasicsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BasicsController extends Controller
+{
+    public function landing() {
+        return view('landing');
+    }
+}
