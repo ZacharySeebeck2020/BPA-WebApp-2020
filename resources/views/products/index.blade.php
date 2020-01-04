6184d61faa3f53f0729e9537f070579958d1f56f
Zachary Seebeck <SeebeckZachary@Zacharys-MacBook-Pro.local>
2020-01-03 22:15:56 -0500

    Work on All Products page.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,16 +15,42 @@
 </div>
 
 <div class="flex">
-    <div class="w-1/3 pl-5 p-5">
-        <h1 class="text-xl text-gray-700 pt-5 font-bold mb-5">Search By Category</h1>
+    <div class="w-3/12 pl-5 p-5 rounded-r-lg border-r-2 border-gray-500">
+        <h1 class="text-xl text-gray-700 pt-5 font-bold mb-3">Search By Category</h1>
         <ul>
             @foreach ($categories as $category)
                 <li><a class="text-lg text-gray-600 my-2" href="#">{{ $category->name }}</a></li>
             @endforeach
         </ul>
     </div>
-    <div class="w-2/3 p-5">
+    <div class="w-9/12 p-5">
+        <div class="w-full py-2 my-2 bg-gray-400 flex">
+            <img src="{{ asset('/img/image-soon.png') }}" class="mx-2 h-40 w-40">
+            <div class="mx-1 w-full h-40 rounded-sm">
 
+            </div>
+        </div>
+
+        <div class="w-full py-2 my-2 bg-gray-400 flex">
+            <img src="{{ asset('/img/image-soon.png') }}" class="mx-2 h-40 w-40">
+            <div class="mx-1 w-full h-40 rounded-sm">
+
+            </div>
+        </div>
+
+        <div class="w-full py-2 my-2 bg-gray-400 flex">
+            <img src="{{ asset('/img/image-soon.png') }}" class="mx-2 h-40 w-40">
+            <div class="mx-1 w-full h-40 rounded-sm">
+
+            </div>
+        </div>
+
+        <div class="w-full py-2 my-2 bg-gray-400 flex">
+            <img src="{{ asset('/img/image-soon.png') }}" class="mx-2 h-40 w-40">
+            <div class="mx-1 w-full h-40 rounded-sm">
+
+            </div>
+        </div>
     </div>
 </div>
 @endsection
