12af8edb9eb2f2c55f7aad41ff90205a8a8b8610
Justin <JustinKruskie@gmail.com>
2020-01-09 10:36:59 -0500

    Dummy Data Seeder
    Created seeder that reads through a .JSON of dummy data.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             \CategoriesSeeder::class,
-            \UsersSeeder::class
+            \UsersSeeder::class,
+            \DummyDataSeeder::class
         ]);
 
 
