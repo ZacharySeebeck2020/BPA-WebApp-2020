b2e20f47dc74bd8b83beaa29480364aed7bc2554
Zachary Seebeck <SeebeckZachary@Zacharys-MacBook-Pro.local>
2020-01-14 11:28:58 -0500

    Fixed product view page.

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -6,28 +6,29 @@
 
 @section('content')
 <div class="">
-    <div class="w-1/2 text-left">
+    <div class="w-full text-left">
         <a href="{{ route('products.index') }}" class="text-gray-400 text-sm">< Back To Products Page</a>
     </div>
 </div>
 
-<div class="p-8 flex mt-4">
-    <div class="w-1/2">
+<div class="p-8 flex md:flex-row flex-col mt-4">
+    <div class="md:w-1/2 w-full">
         @if (isset($product->image))
             <img src="{{ $product->image }}" class="mx-auto w-3/4 shadow-lg rounded-lg">
         @else
             <img src="{{ asset('/img/image-soon.png') }}" class="mx-auto w-3/4 shadow-lg rounded-lg">
         @endif
     </div>
-    <div class="w-1/2 text-left">
-        <h2 class="text-gray-700 text-4xl pt-3">{{ $product->name }}</h2>
-        <h2 class="text-gray-800 text-xl pt-2">${{ $product->price }}</h2>
 
-        <div class="mt-3 text-gray-600 text-lg">
+    <div class="md:w-1/2 w-full text-left">
+        <h2 class="text-gray-700 text-4xl pt-3 md:text-left text-center">{{ $product->name }}</h2>
+        <h2 class="text-gray-800 text-xl pt-2 md:text-left text-center">${{ $product->price }}</h2>
+
+        <div class="mt-3 text-gray-600 text-lg md:text-left text-center">
             {!! $product->description !!}
         </div>
 
-        <div class="mt-10 text-gray-600 text-lg">
+        <div class="mt-10 text-gray-600 text-lg w-full md:text-left text-center">
             <form action="{{ route('cart.add', $product->slug) }}" method="POST">
                 @csrf
                 <button type="submit" class="button button_gray">Add To Cart</button>
@@ -37,7 +38,7 @@
 </div>
 
 @if (!empty($product->details)))
-<div class="px-32 text-left">
+<div class="md:px-32 px-10 text-left">
     <h2 class="text-gray-700 text-2xl pt-3">Details</h2>
     <div class="mt-3 text-gray-600 text-lg">
         {!! $product->details !!}
