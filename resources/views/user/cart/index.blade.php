04470efcc13c5f321f7e6aef2ef6b11be5faaa8e
Zachary Seebeck <SeebeckZachary@Zacharys-MacBook-Pro.local>
2020-01-14 11:36:34 -0500

    Fixed cart page responcitivity.

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -35,12 +35,12 @@
                         <img src="{{ asset('/img/image-soon.png') }}" class="mx-2 mb-2 h-16 w-16 shadow-lg rounded">
                     @endif
                     <div class="mx-1 ml-3 w-full h-30 rounded-sm">
-                        <div class="h-1/4 text-gray-800 text-2xl flex">
+                        <div class="h-1/4 text-gray-800 text-2xl flex md:flex-row flex-col">
                             {{ $product->name }} <span class="text-gray-600 font-bold text-base ml-1">({{ $product->pivot->count }})</span>
-                            <form action="{{ route('cart.remove', $product->id) }}" method="POST" class="ml-auto">
+                            <form action="{{ route('cart.remove', $product->id) }}" method="POST" class="md:ml-auto">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="button_sm button_red text-sm my-auto">Remove One From Cart</button>
+                                <button type="submit" class="button_sm button_red text-sm md:my-auto my-3">Remove One From Cart</button>
                             </form>
                         </div>
                         <div class="h-1/4 text-gray-500 text-sm">
@@ -51,12 +51,12 @@
             </a>
         @endforeach
 
-        <div class="mt-5 w-full flex">
-            <div class="text-gray-800 text-xl">
+        <div class="mt-5 w-full flex md:flex-row flex-col">
+            <div class="text-gray-800 text-xl text-center md:text-right">
                 Total Price: ${{ $cart->getCost() }}
             </div>
 
-            <div class="ml-auto flex">
+            <div class="md:ml-auto md:mt-0 md:mr-0 mr-auto mt-3 flex mx-auto">
                 <h2 class="text-xl text-gray-800 ml-auto">Ready to checkout?</h2>
                 <form action="{{ route('order.start') }}" method="GET">
                     <button type="submit" class="button_sm button_green ml-3">Checkout</button>
